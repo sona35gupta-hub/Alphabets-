2aa270cb20991067a1fa6f1de2411503f9e5956e
Apoorva Gupta <sona35gupta@gmail.com>
2026-05-19 12:29:00 +0530

    alphabets

---
+++ script.js
@@ -0,0 +1,165 @@
+function app(ab)
+{
+    let data1=document.getElementById("data");
+    switch(ab)
+    {
+        case 'A':
+            data1.innerHTML=`
+             <h1>Apple</h1>
+    <h2>A a</h2>
+    <img src="apple.jpg" class="d-block" width="50%">`
+    break;
+    case 'B':
+        data1.innerHTML=`
+             <h1>Banana</h1>
+    <h2>B b</h2>
+    <img src="banana.jpg" class="d-block" width="50%">`
+    break;
+    case 'C':
+        data1.innerHTML=`
+             <h1>Cat</h1>
+    <h2>C  c</h2>
+    <img src="cat.jpg" class="d-block" width="50%">`
+    break;
+     case 'D':
+        data1.innerHTML=`
+             <h1>Dog</h1>
+    <h2>D  d</h2>
+    <img src="dog.jpg" class="d-block" width="50%">`
+    break;
+     case 'E':
+        data1.innerHTML=`
+             <h1>Elephant</h1>
+    <h2>E  e<h2>
+    <img src="elephant.jpg" class="d-block" width="50%">`
+    break;
+     case 'F':
+        data1.innerHTML=`
+             <h1>Fish</h1>
+    <h2>F   f</h2>
+    <img src="Fish.jpg" class="d-block" width="50%">`
+    break;
+    case 'G':
+        data1.innerHTML=`
+             <h1>Girl</h1>
+    <h2>G  g</h2>
+    <img src="girl.jpg" class="d-block" width="50%">`
+    break;
+    case 'H':
+        data1.innerHTML=`
+             <h1>Hen</h1>
+    <h2>H   h</h2>
+    <img src="hen.jpg" class="d-block" width="50%">`
+    break;
+    case 'I':
+        data1.innerHTML=`
+             <h1>Ink</h1>
+    <h2>I  i</h2>
+    <img src="ink.jpg" class="d-block" width="50%">`
+    break;
+    case 'J':
+        data1.innerHTML=`
+             <h1>Jug </h1>
+    <h2>J  j</h2>
+    <img src="jug.jpg" class="d-block" width="50%">`
+    break;
+    case 'K':
+        data1.innerHTML=`
+             <h1>Kite</h1>
+    <h2>K   k</h2>
+    <img src="kite.jpg" class="d-block" width="50%">`
+    break;
+    case 'L':
+        data1.innerHTML=`
+             <h1>Lion</h1>
+    <h2>L  l</h2>
+    <img src="lion.jpg" class="d-block" width="50%">`
+    break;
+case 'M':
+        data1.innerHTML=`
+             <h1>Monkey</h1>
+    <h2>M    m</h2>
+    <img src="monkey.jpg" class="d-block" width="50%">`
+    break;
+    case 'N':
+        data1.innerHTML=`
+             <h1>Nest</h1>
+    <h2>N    n</h2>
+    <img src="nest.jpg" class="d-block" width="50%">`
+    break;
+    case 'O':
+        data1.innerHTML=`
+             <h1>Orange</h1>
+    <h2>O  o</h2>
+    <img src="orange.jpg" class="d-block" width="50%">`
+    break;
+    case 'P':
+        data1.innerHTML=`
+             <h1>parrot</h1>
+    <h2>P  p</h2>
+    <img src="parrot.jpg" class="d-block" width="50%">`
+    break;
+    
+case 'Q':
+        data1.innerHTML=`
+             <h1>Queen</h1>
+    <h2>Q   q</h2>
+    <img src="Queen.jpg" class="d-block" width="50%">`
+    break;
+    case 'R':
+        data1.innerHTML=`
+             <h1>Rat</h1>
+    <h2>R    r</h2>
+    <img src="rat.jpg" class="d-block" width="50%">`
+    break;
+    case 'S':
+        data1.innerHTML=`
+             <h1>Sun</h1>
+    <h2>S  s</h2>
+    <img src="sun.jpg" class="d-block" width="50%">`
+    break;
+    case 'T':
+        data1.innerHTML=`
+             <h1>Train</h1>
+    <h2>T   t</h2>
+    <img src="train.jpg" class="d-block" width="50%">`
+    break;
+    case 'U':
+        data1.innerHTML=`
+             <h1>Umberlla</h1>
+    <h2>U  u</h2>
+    <img src="umberlla.jpg" class="d-block" width="50%">`
+    break;
+    case 'V':
+        data1.innerHTML=`
+             <h1>Van</h1>
+    <h2>V  v</h2>
+    <img src="van.jpg" class="d-block" width="50%">`
+    break;
+    case 'W':
+        data1.innerHTML=`
+             <h1>Watermelon</h1>
+    <h2>F   f</h2>
+    <img src="Watermelon.jpg" class="d-block" width="50%">`
+    break;
+    case 'X':
+        data1.innerHTML=`
+             <h1>X-christmas</h1>
+    <h2>X   x</h2>
+    <img src="christmas.jpg" class="d-block" width="50%">`
+    break;
+    case 'Y':
+        data1.innerHTML=`
+             <h1>Yak</h1>
+    <h2>Y  y </h2>
+    <img src="yak.jpg" class="d-block" width="50%">`
+    break;
+    case 'Z':
+        data1.innerHTML=`
+             <h1>Zebra</h1>
+    <h2>Z   z</h2>
+    <img src="zebra.jpg" class="d-block" width="50%">`
+    break;
+    
+    }
+}
